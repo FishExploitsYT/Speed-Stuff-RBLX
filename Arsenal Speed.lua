cd1c395a2580e4f2ce2360ae1f923b6b6b230c29
FishExploitsYT <72241830+FishExploitsYT@users.noreply.github.com>
2021-03-09 15:45:52 +1000

    Update Arsenal Speed.lua

--- Arsenal Speed.lua
+++ Arsenal Speed.lua
@@ -1,1 +1,7 @@
+--Sup just some Arsenal Speed Script I Found On robloxscripts.com
+--anyway enjoy it
 
+
+game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid"):GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+    setpropvalue(game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid"), "WalkSpeed", 100)
+end)
